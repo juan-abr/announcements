8879e7085f966cd6944603945780ae4d3d6cfeca
senor-green-hill <juanabarro@protonmail.com>
2021-04-10 08:42:15 -0400

    added signup

--- views.py
+++ views.py
@@ -2,8 +2,13 @@
 from django.shortcuts import render, redirect
 from django.http import Http404
 from django.utils.timezone import now
-from django.views.generic.dates import ArchiveIndexView, MonthArchiveView
+from django.urls import reverse_lazy
+
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+
+from django.views.generic.dates import ArchiveIndexView, MonthArchiveView
+from django.views.generic import CreateView
 
 from .models import Announcement, Event, EventRegistration
 
@@ -52,3 +57,13 @@ def event_add_attendance(request, pk):
     this_event = Events.objects.get(pk=pk)
     this_event.add_user_to_list_of_attendees(user = request.user)
     return redirect('announcements:event_month', pk=pk)
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
+
+# class EventRegistrationView(CreateView):
+#     form_class = EventRegistrationForm
+#     success_url = 
+#     template_name =
